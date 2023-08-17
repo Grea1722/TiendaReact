9480d987adfc783a82f9dbbd06431acda86ee0e5
Grea1722 <alexs17@live.com.mx>
2023-08-17 03:33:18 -0800

    Firts commit

---
+++ src/components/Filters.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import "./Filters.css";
+
+export const Filters = ({ onChange }) => {
+  const [minPrice, setMinPrice] = useState(0);
+
+  const handleChangeMinPrice = (event) => {
+    setMinPrice(event.target.value);
+    onChange((prevState) => ({
+      ...prevState,
+      minPrice: event.target.value,
+    }));
+  };
+
+  const handleChangeCategory = (event) => {
+    onChange((prevState) => ({
+      ...prevState,
+      category: event.target.value,
+    }));
+  };
+  return (
+    <section className="filters">
+      <div>
+        <label htmlFor="price">Price</label>
+        <input
+          type="range"
+          id="price"
+          min="0"
+          max="1000"
+          onChange={handleChangeMinPrice}
+        />
+        <span>{minPrice}</span>
+      </div>
+
+      <div>
+        <label htmlFor="category">Categoria</label>
+        <select name="" id="category" onChange={handleChangeCategory}>
+          <option value="all">Todas</option>
+          <option value="laptops">Portatiles</option>
+          <option value="smartphones">Celulares</option>
+        </select>
+      </div>
+    </section>
+  );
+};
